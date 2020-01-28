355cdd7e292da52bfd24fc5c3f4a59f295113ab5
Seid Mehmedovic <mehmedovic.seid@gmail.com>
2020-01-28 19:05:40 +0100

    formatting code

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,27 +1,24 @@
-import { Component, OnInit } from "@angular/core";
+import { Component, OnInit } from '@angular/core';
 import { Router, ActivatedRoute, RoutesRecognized, NavigationEnd } from '@angular/router';
 
 @Component({
-  selector: "app-root",
-  templateUrl: "./app.component.html",
-  styleUrls: ["./app.component.scss"]
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent implements OnInit {
-
   currentUrl = '';
-  isLocalhost = false;
+  showHeaderAndSidebar = false;
 
-  constructor(
-    private _router: Router,
-    private _activatedRoute: ActivatedRoute
-  ) { }
+  constructor(private _router: Router, private _activatedRoute: ActivatedRoute) {}
 
   ngOnInit(): void {
     // this._activatedRoute.data.subscribe((data: any) => {
     //   console.log(data);
     // });
 
-    this.isLocalhost = window.location.hostname === 'localhost';
+    this.showHeaderAndSidebar =
+      window.location.hostname === 'localhost' || window.location.hostname === 'flxng.codeeve.com';
 
     this._router.events.subscribe(event => {
       /* Event classes: NavigationStart, RoutesRecognized, NavigationEnd, NavigationCancel and NavigationError */
