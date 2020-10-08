88701a54450bef4acb50d36963dd657b8caadf4b
Seid Mehmedovic <mehmedovic.seid@gmail.com>
2020-10-08 06:37:59 +0200

    multiple size options for hamburger

--- src/app/shared/components/hamburger/hamburger.component.ts
+++ src/app/shared/components/hamburger/hamburger.component.ts
@@ -1,13 +1,22 @@
 import { Component, OnInit, Input, EventEmitter, Output } from '@angular/core';
 
+export enum HamburgerSize {
+  Medium = 'medium',
+  Large = 'large',
+}
+
 @Component({
   selector: 'app-hamburger',
   templateUrl: './hamburger.component.html',
   styleUrls: ['./hamburger.component.scss'],
 })
 export class HamburgerComponent implements OnInit {
-  @Input() open = false;
   @Output() onToggle = new EventEmitter<boolean>();
+
+  @Input() open = false;
+  @Input() size: HamburgerSize = HamburgerSize.Medium;
+
+  HamburgerSize = HamburgerSize;
 
   constructor() {}
 
