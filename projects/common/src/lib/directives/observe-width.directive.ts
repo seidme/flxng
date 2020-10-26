b81cd3b10a5eb55ee654f6c23a3f0e5242ee6999
Seid Mehmedovic <mehmedovic.seid@gmail.com>
2020-10-26 04:20:49 +0100

    Replacing Renderer -> Renderer2 import in common package

--- projects/common/src/lib/directives/observe-width.directive.ts
+++ projects/common/src/lib/directives/observe-width.directive.ts
@@ -1,83 +1,71 @@
 import {
-    Directive,
-    Input,
-    Output,
-    NgZone,
-    Renderer,
-    ElementRef,
-    OnInit,
-    OnChanges,
-    AfterViewChecked,
-    SimpleChanges,
-    EventEmitter
+  Directive,
+  Input,
+  Output,
+  NgZone,
+  Renderer2,
+  ElementRef,
+  OnInit,
+  OnChanges,
+  AfterViewChecked,
+  SimpleChanges,
+  EventEmitter,
 } from '@angular/core';
 
 @Directive({
-    selector: '[flxObserveWidth]'
+  selector: '[flxObserveWidth]',
 })
 export class ObserveWidthDirective implements OnInit, OnChanges, AfterViewChecked {
+  @Input('flxObserveWidth') shouldObserve = false;
 
-    @Input('flxObserveWidth') shouldObserve: boolean = false;
+  @Output() onWidthChange = new EventEmitter<string>();
 
-    @Output() onWidthChange: EventEmitter<string> = new EventEmitter<string>();
+  width = '';
+  windowResizeListener: any;
 
-    width: string = '';
-    windowResizeListener: any;
+  constructor(private _ngZone: NgZone, private _renderer: Renderer2, private _elementRef: ElementRef) {}
 
-    constructor(
-        private _ngZone: NgZone,
-        private _renderer: Renderer,
-        private _elementRef: ElementRef
-    ) { }
+  ngOnInit(): void {}
 
+  ngOnChanges(changes: SimpleChanges): void {
+    if (changes.shouldObserve) {
+      if (changes.shouldObserve.currentValue) {
+        //this._ngZone.runOutsideAngular(() => {
+        //setTimeout(() => {
+        this.width = getComputedStyle(this._elementRef.nativeElement).width;
+        this.onWidthChange.emit(this.width);
 
-    ngOnInit(): void {
-
-    }
-
-    ngOnChanges(changes: SimpleChanges): void {
-        if(changes.shouldObserve) {
-            if(changes.shouldObserve.currentValue) {
-                //this._ngZone.runOutsideAngular(() => {
-                    //setTimeout(() => {
-                        this.width = getComputedStyle(this._elementRef.nativeElement).width;
-                        this.onWidthChange.emit(this.width);
-
-                        this.windowResizeListener = this._renderer.listen(window, 'resize', (e) => {
-                            let width = getComputedStyle(this._elementRef.nativeElement).width;
-                            if(this.width !== width) {
-                                this.width = width;
-                                this.onWidthChange.emit(this.width);
-                            }
-                        });
-                    //});
-                //});
-            }
-            else {
-                if(this.width) {
-                    this.width = '';
-                    this.onWidthChange.emit(this.width);
-                }
-
-                if(this.windowResizeListener) {
-                    this.windowResizeListener() // unbind
-                }
-            }
-        }
-    }
-
-    ngAfterViewChecked(): void {
-        if(!this.shouldObserve) {
-            return;
-        }
-
-        let width = getComputedStyle(this._elementRef.nativeElement).width;
-        if(width !== this.width) {
+        this.windowResizeListener = this._renderer.listen(window, 'resize', (e) => {
+          const width = getComputedStyle(this._elementRef.nativeElement).width;
+          if (this.width !== width) {
             this.width = width;
             this.onWidthChange.emit(this.width);
-       }
+          }
+        });
+        //});
+        //});
+      } else {
+        if (this.width) {
+          this.width = '';
+          this.onWidthChange.emit(this.width);
+        }
+
+        if (this.windowResizeListener) {
+          this.windowResizeListener(); // unbind
+        }
+      }
+    }
+  }
+
+  ngAfterViewChecked(): void {
+    if (!this.shouldObserve) {
+      return;
     }
 
-
-
+    const width = getComputedStyle(this._elementRef.nativeElement).width;
+    if (width !== this.width) {
+      this.width = width;
+      this.onWidthChange.emit(this.width);
+    }
+  }
 }
