90aad98953e5b3de75b9d8b91894d4427a46ea09
Seid Mehmedovic <mehmedovic.seid@gmail.com>
2020-10-27 02:35:07 +0100

    replacing custom templateLoader implementation with Angular built-in template loader (*ngTemplateOutlet)

--- projects/treetable/src/lib/shared/constants/grid-templates.ts
+++ projects/treetable/src/lib/shared/constants/grid-templates.ts
@@ -1,8 +1,8 @@
 export class GridTemplates {
-    static readonly options: string = 'options';
-    static readonly header: string = 'header';
-    static readonly headContextMenu: string = 'headContextMenu';
-    static readonly rowExpansion: string = 'rowExpansion';
-    static readonly noResults: string = 'noResults';
-    static readonly footer: string = 'footer';
+    static readonly options = 'options';
+    static readonly header = 'header';
+    static readonly headContextMenu = 'headContextMenu';
+    static readonly rowExpansion = 'rowExpansion';
+    static readonly noResults = 'noResults';
+    static readonly footer = 'footer';
 }
