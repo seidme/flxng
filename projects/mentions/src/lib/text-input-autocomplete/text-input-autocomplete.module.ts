1242be4d197adbb472fe206f7161f6d4c39d35b4
Seid Mehmedovic <mehmedovic.seid@gmail.com>
2021-01-25 01:38:01 +0100

    mentions - remove unused components

--- projects/mentions/src/lib/text-input-autocomplete/text-input-autocomplete.module.ts
+++ projects/mentions/src/lib/text-input-autocomplete/text-input-autocomplete.module.ts
@@ -2,19 +2,16 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { TextInputAutocompleteComponent } from './text-input-autocomplete.component';
-import { TextInputAutocompleteContainerComponent } from './text-input-autocomplete-container.component';
 import { TextInputAutocompleteMenuComponent } from './text-input-autocomplete-menu.component';
 
 @NgModule({
   declarations: [
     TextInputAutocompleteComponent,
-    TextInputAutocompleteContainerComponent,
     TextInputAutocompleteMenuComponent
   ],
   imports: [CommonModule],
   exports: [
     TextInputAutocompleteComponent,
-    TextInputAutocompleteContainerComponent,
     TextInputAutocompleteMenuComponent
   ],
   entryComponents: [TextInputAutocompleteMenuComponent]
